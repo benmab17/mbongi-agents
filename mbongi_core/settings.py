0c33ccad0f625c668fdccd7704f7b6c56dd7d9d4
Ben Mab <bienvenumabukama@gmail.com>
2026-01-15 11:08:55 +0100

    Use DATABASE_URL for database configuration

--- mbongi_core/settings.py
+++ mbongi_core/settings.py
@@ -106,12 +106,12 @@ TEMPLATES = [
 # DATABASE
 # =========================
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
-
 
 # =========================
 # AUTH / LOGIN
